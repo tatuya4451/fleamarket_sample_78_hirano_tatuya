239064f0466b3ff4359fdf8021d2eeedefec0aa6
kumapaaan <kumapaaan@users.noreply.github.com>
2020-07-07 20:52:27 +0900

    Generate user model
    Generate user model related to devise using rails task.
    
    Actual command: rails g devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'items#index'
   resources :items, only: [:index, :show, :new] do
     member do
       get 'purchase'
     end
   end
-
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
 end

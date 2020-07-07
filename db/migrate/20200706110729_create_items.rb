6d8ed6f7a9249e98cab51ee91319227fe650cae8
Yuzuru-E <yzryzr88@gmail.com>
2020-07-07 18:08:56 +0900

    create_items重複修正

--- db/migrate/20200706110729_create_items.rb
+++ db/migrate/20200706110729_create_items.rb
@@ -8,6 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id, null: false
       t.integer :preparation_id, null: false
       t.integer :price, null: false
+      t.references :category, null:false
       t.timestamps
     end
   end

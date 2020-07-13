62dac23d379b314f008a8de543685321ac6b9972
tatuya4451 <tatuya44511336@gmail.com>
2020-07-13 17:04:31 +0900

    category修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_07_11_062846) do
 
-
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_last_name", null: false
     t.string "destination_first_name", null: false
@@ -29,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_07_11_062846) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

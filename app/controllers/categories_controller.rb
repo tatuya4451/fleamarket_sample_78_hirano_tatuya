1979af10c9af9e624986b0f6db80d5617436a5ea
Yuzuru-E <yzryzr88@gmail.com>
2020-07-21 18:33:40 +0900

    パンくずリスト(追加)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,9 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: :show
   before_action :set_item_search_query
+  before_action :set_parent
 
   def index
-    @parents = Category.where(ancestry: nil)
-  end
-
-  def show
-    @items = @category.set_items
-    @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
-  end
-
-  private
-  def set_category
-    @category = Category.find(params[:id])
   end
 
 end
